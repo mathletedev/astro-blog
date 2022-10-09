716eb85c24820a1ff61d3f9c966f863652963920
Neal Wang <nealwang.sh@protonmail.com>
2022-10-08 23:33:01 -0700

    idk

---
+++ src/components/Counter.tsx
@@ -0,0 +1,13 @@
+// import { FC, useState } from "react";
+
+// const Counter: FC = () => {
+// 	// const [counter, setCounter] = useState(0);
+
+// 	// return <button onClick={() => setCounter(counter + 1)}>{counter}</button>;
+// 	return <div>hello</div>;
+// };
+
+// export default Counter;
+export default function () {
+	return <div>Hello!</div>;
+}
